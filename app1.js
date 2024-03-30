8e920ae9829fdf235bdbd622d92b2941944d5cf1
Dommini <atordomin2@gmail.com>
2024-03-30 02:47:30 +0100

    tabela fajna

---
+++ app1.js
@@ -0,0 +1,30 @@
+// Pobierz wszystkie divy o klasie buttonTitle
+var buttonTitles = document.querySelectorAll('.buttonTitle')
+
+// Iteruj przez każdy div buttonTitle
+buttonTitles.forEach(function (buttonTitle) {
+  // Pobierz kolejny element po divie buttonTitle, który jest tabelą
+  var table = buttonTitle.nextElementSibling
+
+  // Dodaj klasę "hidden" do tabeli, aby ją ukryć na początku
+  table.classList.add('hidden')
+
+  // Dodaj nasłuchiwanie zdarzenia kliknięcia
+  buttonTitle.addEventListener('click', function () {
+    // Toggle klasy "hidden" dla tabeli
+    table.classList.toggle('hidden')
+
+    // Zmieniaj ikonę "+" na "-" (i na odwrót) dla diva .expand-icon
+    var expandIcon = buttonTitle.querySelector('.expand-icon')
+    expandIcon.textContent = expandIcon.textContent === '▼' ? '▲' : '▼'
+
+    // Dodaj obrót dla ikony po kliknięciu
+    if (expandIcon.textContent === '▲') {
+      expandIcon.style.transition = 'transform 0.7s ease' // Ustawienie płynności transformacji
+      expandIcon.style.transform = 'rotate(360deg)' // Obrót ikony
+    } else {
+      expandIcon.style.transition = 'transform 0.7s ease' // Ustawienie płynności transformacji
+      expandIcon.style.transform = 'rotate(0deg)' // Obrót ikony
+    }
+  })
+})
