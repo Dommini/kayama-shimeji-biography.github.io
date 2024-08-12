c3b322edba0a58ed08c7617d6ddf16cf01b5421b
Dominik Garbaciok <98327208+Dommini@users.noreply.github.com>
2024-08-13 01:46:29 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -16,3 +16,45 @@ document.addEventListener('DOMContentLoaded', function () {
     powiekszoneZdjecie.style.display = 'none'
   })
 })
+document.addEventListener('DOMContentLoaded', function () {
+  var link = document.createElement('link')
+  link.rel = 'icon'
+  link.type = 'image/gif'
+  link.href = 'img/full_x3_happy.gif' // Ścieżka do Twojego GIF-a
+  document.head.appendChild(link)
+})
+document.addEventListener('DOMContentLoaded', function () {
+  const themeToggleButton = document.getElementById('theme-toggle')
+  const currentTheme = localStorage.getItem('theme')
+  const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)')
+    .matches
+
+  function applyDarkMode(darkMode) {
+    if (darkMode) {
+      document.body.classList.add('dark-mode')
+      document
+        .querySelectorAll(
+          'button, table, th, td, h1, h2, h4, h5, a, input, textarea',
+        )
+        .forEach((element) => element.classList.add('dark-mode'))
+    } else {
+      document.body.classList.remove('dark-mode')
+      document
+        .querySelectorAll(
+          'button, table, th, td, h1, h2, h4, h5, a, input, textarea',
+        )
+        .forEach((element) => element.classList.remove('dark-mode'))
+    }
+  }
+
+  if (currentTheme === 'dark' || (!currentTheme && prefersDarkScheme)) {
+    applyDarkMode(true)
+  }
+
+  themeToggleButton.addEventListener('click', function () {
+    const darkModeEnabled = document.body.classList.toggle('dark-mode')
+    applyDarkMode(darkModeEnabled)
+    const theme = darkModeEnabled ? 'dark' : 'light'
+    localStorage.setItem('theme', theme)
+  })
+})
