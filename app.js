d5d2e055650cfeb09da95de93b1b1794381813de
Dommini <atordomin2@gmail.com>
2024-11-11 02:38:19 +0100

    last changes

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const miniatury = document.querySelectorAll('.zdjecie')
-  const powiekszoneZdjecie = document.querySelector('.powiekszone-zdjecie')
-  const zdjecie = powiekszoneZdjecie.querySelector('.powiekszenie')
-  const zamknij = powiekszoneZdjecie.querySelector('.zamknij')
+  const buttonTitles = document.querySelectorAll('.buttonTitle')
 
-  miniatury.forEach(function (miniatura) {
-    miniatura.addEventListener('click', function () {
-      const src = this.getAttribute('data-src')
-      zdjecie.setAttribute('src', src)
-      powiekszoneZdjecie.style.display = 'block'
+  buttonTitles.forEach(function (buttonTitle) {
+    buttonTitle.addEventListener('click', function () {
+      const table = this.nextElementSibling
+      const icon = this.querySelector('.expand-icon')
+
+      if (table.classList.contains('hidden')) {
+        table.classList.remove('hidden')
+        table.classList.add('visible')
+        icon.style.transform = 'rotate(180deg)'
+      } else {
+        table.classList.remove('visible')
+        table.classList.add('hidden')
+        icon.style.transform = 'rotate(0deg)'
+      }
     })
-  })
-
-  zamknij.addEventListener('click', function () {
-    powiekszoneZdjecie.style.display = 'none'
   })
 })
